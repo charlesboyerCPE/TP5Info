c06abc2965cbc352f2cdd6be7a2492492bf370c8
Atma021 <charles.boyer021@gmail.com>
2020-12-12 15:48:41 +0100

    Exercice 2

--- repertoire.c
+++ repertoire.c
@@ -1,6 +1,7 @@
 #include <dirent.h> 
 #include <stdio.h> 
 #include <stdlib.h>
+#include <string.h>
 #include "repertoire.h"
 
 void exercice1()
@@ -13,30 +14,80 @@ void exercice1()
     scanf("%s", cheminDossier);
 
     lire_dossier(cheminDossier);
+
+    free (cheminDossier);
+}
+
+void exercice2()
+{
+    //On alloue la mémorie pour scanf
+    char *cheminDossier = malloc(sizeof(char));
+
+    //Saisi de l'utilisateur
+    printf("Veuillez saisir le chemin du dossier: ");
+    scanf("%s", cheminDossier);
+
+    lire_dossier_recursif(cheminDossier);
+
+    free (cheminDossier);
 }
 
 void lire_dossier(char *cheminDossier)
 {
     DIR *dossier = NULL;
-    struct dirent *dir = NULL;
+    struct dirent *entree = NULL;
     
     //Ouverture du dossier
     dossier = opendir(cheminDossier);
     if (dossier != NULL) 
     {
         printf("Fichier et dossier dans %s:\n", cheminDossier);
-        while ((dir = readdir(dossier)) != NULL) 
+        while ((entree = readdir(dossier)) != NULL) 
         {
-            printf("%s\n", dir->d_name); //La structure dirent possède un champ pour le nom du fichier
+            printf("%s\n", entree->d_name); //La structure dirent possède un champ pour le nom du fichier
         }
 
         closedir(dossier);
     }
 }
 
+void lire_dossier_recursif(char *cheminDossier)
+{
+    DIR *dossier = NULL;
+    struct dirent *entree = NULL;
+    char *cheminActuel = malloc(1000 * sizeof(char));
+
+    //Ouverture du dossier
+    dossier = opendir(cheminDossier);
+    if (dossier != NULL) 
+    {
+        printf("%s:\n", cheminDossier);
+
+        //On affiche les choses présentes dans le dossier
+        while ((entree = readdir(dossier)) != NULL) 
+        {
+            //On enlève les . et ..
+            if (strcmp(entree->d_name, ".") != 0 && strcmp(entree->d_name, "..") != 0)
+            {
+                printf("\t%s\n", entree->d_name);
+
+                // On construit un nouveau chemin
+                strcpy(cheminActuel, cheminDossier);
+                strcat(cheminActuel, "/");
+                strcat(cheminActuel, entree->d_name);
+
+                lire_dossier_recursif(cheminActuel);
+            }
+        }
+        closedir(dossier);
+    }
+
+    free (cheminActuel);
+}
+
 int main(void) 
 {
-    
+
 
     return(0);
 }
--- repertoire.h
+++ repertoire.h
@@ -10,9 +10,22 @@ Paramètre: Le chemin du dossier
 void lire_dossier(char *);
 
 /*
+Nom: void lire_dossier_recursif(char *)
+Détail: Procédure permettant de lire un dossiers et d'afficher
+        les fichiers/répertoires présents de facon récurcive (comme "ls -R")
+Paramètre: Le chemin du dossier
+*/
+void lire_dossier_recursif(char *);
+
+/*
 Nom: void exercice1()
 Détail: Procédure contenant l'exercice 1
 */
 void exercice1();
 
+/*
+Nom: void exercice2()
+Détail: Procédure contenant l'exercice 2
+*/
+void exercice2();
 #endif
